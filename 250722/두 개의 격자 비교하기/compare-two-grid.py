b50984dbd214edf658ccd44425f08e530c0146e9
민형 <wemin05@gmail.com>
2025-07-22 22:19:01 +0900

    Title: 두 개의 격자 비교하기, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250722/두 개의 격자 비교하기/compare-two-grid.py
+++ 250722/두 개의 격자 비교하기/compare-two-grid.py
@@ -2,13 +2,8 @@ n,m=map(int, input().split())
 arr1=[list(map(int,input().split())) for _ in range(n)]
 arr2=[list(map(int, input().split())) for _ in range(n)]
 arr3=[
-    [0 for _ in range(n+1)]
-    for _ in range(m+1)
-]
-for i in range(n):
-    for j in range(m):
-        if arr1[i][j]!=arr2[i][j]:
-            arr3[i][j]=1
+    [1 if arr1[i][j] != arr2[i][j] else 0 for j in range(m)]
+    for i in range(n)]
 
 for i in range(n):
     for j in range(m):
