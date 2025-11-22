2c51d1ce00dda8de99371972f618d9028d482d42
민형 <wemin05@gmail.com>
2025-11-22 21:04:17 +0900

    Title: 배열의 평균, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251122/배열의 평균/ave-of-array.py
+++ 251122/배열의 평균/ave-of-array.py
@@ -5,14 +5,14 @@ for i in range(2):
     for j in range(4):
         sumg+=arr_2d[i][j]
         sumall+=arr_2d[i][j]
-    print(sumg/4,end=" ")
+    print(f"{sumg/4:.1f}",end=" ")
 print()
 
 for i in range(4):
     sums=0
     for j in range(2):
         sums+=arr_2d[j][i]
-    print(sums/2,end=" ")
+    print(f"{sums/2:.1f}",end=" ")
 print()
 
-print(sumall/8)
+print(f"{sumall/8:.1f}")
