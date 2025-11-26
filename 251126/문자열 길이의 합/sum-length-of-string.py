c7a126293745e59a5bb872449302e1f104a5cd0b
민형 <wemin05@gmail.com>
2025-11-26 17:53:34 +0900

    Title: 문자열 길이의 합, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251126/문자열 길이의 합/sum-length-of-string.py
+++ 251126/문자열 길이의 합/sum-length-of-string.py
@@ -1,12 +1,9 @@
 n=int(input())
-arr1=input()
-arr2=input()
-arr3=input()
 count=0
-if arr1[0]=='a':
-    count+=1
-if arr2[0]=='a':
-    count+=1
-if arr3[0]=='a':
-    count+=1
-print(len(arr1)+len(arr2)+len(arr3),count)
+sum=0
+for i in range(n):
+    arr=input()
+    sum+=len(arr)
+    if arr[0]=="a":
+        count+=1
+print(sum, count)
