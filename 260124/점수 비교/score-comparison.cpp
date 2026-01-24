ef6a2c21ab60ce426089a8049c3abd4f76203d9a
민형 <wemin05@gmail.com>
2026-01-24 19:24:51 +0900

    Title: 점수 비교, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260124/점수 비교/score-comparison.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int ma,ea,mb,eb;
+    cin>>ma>>ea>>mb>>eb;
+    if(ma>mb&&ea>eb){
+        cout<<1;
+    }
+    else{
+        cout<<0;
+    }
+    // Please write your code here.
+    return 0;
+}
