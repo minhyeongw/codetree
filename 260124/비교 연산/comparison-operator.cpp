f358ea53d541129442c452d8416d95c633ae9a82
민형 <wemin05@gmail.com>
2026-01-24 18:55:38 +0900

    Title: 비교 연산, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260124/비교 연산/comparison-operator.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+    if (a>=b) {
+        cout<<1<<endl;
+    }
+    else{
+        cout<<0<<endl;
+    }
+    if (a>b) {
+        cout<<1<<endl;
+    }
+    else{
+        cout<<0<<endl;
+    }
+    if (b>=a) {
+        cout<<1<<endl;
+    }
+    else{
+        cout<<0<<endl;
+    }
+    if (b>a) {
+        cout<<1<<endl;
+    }
+    else {
+        cout<<0<<endl;
+    }
+    if (a==b) {
+        cout<<1<<endl;
+    }
+    else{
+        cout<<0<<endl;
+    }
+    if (a!=b) {
+        cout<<1<<endl;
+    }
+    else{
+        cout<<0<<endl;
+    }
+    // Please write your code here.
+    return 0;
+}
