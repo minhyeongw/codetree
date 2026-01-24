bc711abb2120c85e1dd293a57c379741b522ec6a
민형 <wemin05@gmail.com>
2026-01-24 18:35:25 +0900

    Title: 달에서 무게 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260124/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int w=13;
+    double g_in_moon=0.165;
+    cout<<fixed;
+    cout.precision(6);
+    cout<<w<<" * "<<g_in_moon<<" = "<<w*g_in_moon;
+    
+    // Please write your code here.
+    return 0;
+}
