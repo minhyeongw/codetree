603649c0fb1c7b80758b8aef53f9dc2d0132456c
민형 <wemin05@gmail.com>
2026-01-24 18:48:01 +0900

    Title: 합과 평균의 차, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260124/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin>>a>>b>>c;
+    int total=a+b+c;
+    int avg=total/3;
+    cout<<total<<"\n"<<avg<<"\n"<<total-avg;
+    // Please write your code here.
+    return 0;
+}
