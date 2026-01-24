196b7fea02dfc0a11600693eb81508d249def927
민형 <wemin05@gmail.com>
2026-01-24 19:31:52 +0900

    Title: 윤년인가, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260124/윤년인가/is-leap-year.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int y;
+    cin>>y;
+
+    if((y%4!=0)||(y%100==0 && y%400!=0)){
+        cout<<"false";
+    }
+    else{
+        cout<<"true";
+    }
+    // Please write your code here.
+    return 0;
+}
