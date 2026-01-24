def01fddffe9c78761192d583f4cd0a15e7437ef
민형 <wemin05@gmail.com>
2026-01-24 19:15:27 +0900

    Title: 정수 입력받아 배수 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260124/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int a=0;
+    cin>>n;
+    for(int i=0;i<5;i++){
+        a+=n;
+        cout<<a<<" ";
+    }
+    // Please write your code here.
+    return 0;
+}
