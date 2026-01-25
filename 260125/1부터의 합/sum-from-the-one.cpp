343fab3ece480e3b1a29e7e4f51e9b6dd721ee81
민형 <wemin05@gmail.com>
2026-01-25 22:07:32 +0900

    Title: 1부터의 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260125/1부터의 합/sum-from-the-one.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int sum=0;
+    for(int i=1;i<=100;i++){
+        sum+=i;
+        if(sum>=n){
+            cout<<i;
+            break;
+        }
+    }
+    // Please write your code here.
+    return 0;
+}
