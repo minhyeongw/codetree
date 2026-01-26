8c2e1aa3a19a35a503e4094751326d90512c63a8
민형 <wemin05@gmail.com>
2026-01-26 17:00:31 +0900

    Title: 상하로 반복하며 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260126/상하로 반복하며 출력/print-in-up-down-reverse.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int arr[n][n];
+
+    for(int i=0;i<n;i++){
+        int cnt=1;
+        if(i%2==0){
+            for(int j=0;j<n;j++){
+                arr[j][i]=cnt;
+                cnt+=1;
+            }
+        }
+        else{
+            for(int j=n-1;j>=0;j--){
+                arr[j][i]=cnt;
+                cnt+=1;
+            }
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<arr[i][j];
+        }
+        cout<<endl;
+    }
+    // Please write your code here.
+    return 0;
+}
