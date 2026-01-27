1e18b54636ca75f9eebac812cc8460293d1bdd65
민형 <wemin05@gmail.com>
2026-01-27 19:34:46 +0900

    Title: 전화번호 바꾸기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260127/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,y,x;
+    char k;
+    cin>>a>>k>>x>>k>>y;
+    cout<<0<<a<<"-"<<y<<"-"<<x;
+
+    // Please write your code here.
+    return 0;
+}
