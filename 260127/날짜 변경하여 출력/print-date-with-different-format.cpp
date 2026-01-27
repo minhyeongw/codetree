0dea90a1c809cebc198ec534a1d7e134abdcdd13
민형 <wemin05@gmail.com>
2026-01-27 19:31:44 +0900

    Title: 날짜 변경하여 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260127/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int y,m,d;
+    char k;
+    cin>>y>>k>>m>>k>>d;
+    cout<<m<<"-"<<d<<"-"<<y;
+    // Please write your code here.
+    return 0;
+}
