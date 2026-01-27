34b253742791be25ff7d8d6e44781a5ee695f2c6
민형 <wemin05@gmail.com>
2026-01-27 19:27:25 +0900

    Title: 1시간 뒤 시간 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260127/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int h,m;
+    cin>>h;
+    cin.get();
+    cin>>m;
+    cout<<h+1<<":"<<m;
+    // Please write your code here.
+    return 0;
+}
