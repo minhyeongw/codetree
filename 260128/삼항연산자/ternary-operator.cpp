e5c0f82969e0e6c65610165d270dc003aec8295b
민형 <wemin05@gmail.com>
2026-01-28 16:38:16 +0900

    Title: 삼항연산자, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260128/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    string k= n==100 ? "pass" : "failure";
+    cout<<k;
+    // Please write your code here.
+    return 0;
+}
