ffe3552cd4b8802be7f0c7769e73b23c688530d8
민형 <wemin05@gmail.com>
2026-01-28 16:35:29 +0900

    Title: 2개 중 최대, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260128/2개 중 최대/max-of-two-nums.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+    int max=a>b ? a : b;
+    cout<<max;
+    // Please write your code here.
+    return 0;
+}
