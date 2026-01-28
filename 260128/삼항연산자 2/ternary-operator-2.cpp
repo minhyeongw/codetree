f387b4164da13caec81bd315912311ebcbd396f7
민형 <wemin05@gmail.com>
2026-01-28 16:41:19 +0900

    Title: 삼항연산자 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260128/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin>>a;
+    char k= a==1 ? 't' : 'f';
+    cout<<k;
+    // Please write your code here.
+    return 0;
+}
