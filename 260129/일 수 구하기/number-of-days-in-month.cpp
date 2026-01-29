23533ee482c88ca51017c89b21db4d27388dc561
민형 <wemin05@gmail.com>
2026-01-29 15:17:11 +0900

    Title: 일 수 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260129/일 수 구하기/number-of-days-in-month.cpp
+++ 260129/일 수 구하기/number-of-days-in-month.cpp
@@ -4,15 +4,26 @@ using namespace std;
 int main() {
     int n;
     cin>>n;
-    if (n%2==1){
+    if (n<=7){
+        if (n%2==1){
         cout<<31;
-    }
-    else if (n!=2){
+        }
+        else if (n!=2){
         cout<<30;
+        }
+        else{
+        cout<<28;
+        }
     }
     else{
-        cout<<28;
+        if (n%2==0){
+            cout<<31;
+        }
+        else{
+            cout<<30;
+        }
     }
+    
     // Please write your code here.
     return 0;
 }
