c9de88b2d7723884d7ef39394df14f6475da2a1c
민형 <wemin05@gmail.com>
2026-01-30 14:40:14 +0900

    Title: N까지 자연수 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260130/N까지 자연수 출력/print-1-to-n.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int a=1;
+    while(a<=n){
+        cout<<a<<" ";
+        a++;
+    }
+    // Please write your code here.
+    return 0;
+}
