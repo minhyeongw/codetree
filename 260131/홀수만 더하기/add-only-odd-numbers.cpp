a64840f721fdc2ce71f83702b5cf03bb6fe22823
민형 <wemin05@gmail.com>
2026-01-31 17:43:20 +0900

    Title: 홀수만 더하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260131/홀수만 더하기/add-only-odd-numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,a,sum=0;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>a;
+        if (a%2==1 && a%3==0){
+            sum+=a;
+        }
+   
+    }
+    cout<<sum;
+    // Please write your code here.
+    return 0;
+}
