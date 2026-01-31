39d6832536f51ee81776087c82d6c0d3d0259b6e
민형 <wemin05@gmail.com>
2026-01-31 18:06:22 +0900

    Title: 친근하지 않은 수, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260131/친근하지 않은 수/unfriendly-number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, cnt=0;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        if (i%2==0 || i%3==0)
+    }
+    // Please write your code here.
+    return 0;
+}
