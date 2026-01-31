00621afc21995073edb5552560ee3d91704a7754
민형 <wemin05@gmail.com>
2026-01-31 18:28:25 +0900

    Title: 출석 부르기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260131/출석 부르기 2/calling-attendance-2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    while(1){
+        if (n==1){
+            cout<<"John"<<endl;
+        }
+        else if (n==2){
+            cout<<"Tom"<<endl;
+        }
+        else if (n==3){
+            cout<<"Paul"<<endl;
+        }
+        else if (n==4){
+            cout<<"Sam"<<endl;
+        }
+        else{
+            cout<<"Vacancy"<<endl;
+            break;
+        }
+        cin>>n;
+    }
+    // Please write your code here.
+    return 0;
+}
