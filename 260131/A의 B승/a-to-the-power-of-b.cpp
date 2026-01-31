6b08fd9d6e3c9cdbc5cfcc6c06329ce1b56b686f
민형 <wemin05@gmail.com>
2026-01-31 18:02:10 +0900

    Title: A의 B승, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260131/A의 B승/a-to-the-power-of-b.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+    int prod=1;
+    for(int i=0;i<b;i++){
+        prod*=a;
+    }
+    cout<<prod;
+    // Please write your code here.
+    return 0;
+}
