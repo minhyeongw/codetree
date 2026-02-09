76a2822278aa594f5b8751915587c3f1684f52f8
민형 <wemin05@gmail.com>
2026-02-09 16:15:52 +0900

    Title: 3N + 1 수열, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260209/3N + 1 수열/3n-plus-1-sequence.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,cnt=0;
+    cin>>n;
+    while(n!=1){
+        if (n%2==0){
+            n/=2;
+        }
+        else{
+            n=n*3+1;
+        }
+        cin>>n;
+        cnt++;
+    }
+    cout<<cnt;
+    // Please write your code here.
+    return 0;
+}
