1b9eec89d2198fcea0e7b2ec669113971da951ca
민형 <wemin05@gmail.com>
2026-02-09 16:42:48 +0900

    Title: ab 사이에 없는 c, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260209/ab 사이에 없는 c/c-between-a-and-b-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin>>a>>b>>c;
+    bool stat=false;
+    for(int i=a;i<=b;i++){
+        if (c%i==0){
+            stat=true;
+        }
+    }
+    if (stat==false){
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+    // Please write your code here.
+    return 0;
+}
