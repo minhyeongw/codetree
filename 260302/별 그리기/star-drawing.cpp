efae5369ce2ca610790c94bff6338e0bc83e9eea
민형 <wemin05@gmail.com>
2026-03-02 13:46:35 +0900

    Title: 별 그리기, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260302/별 그리기/star-drawing.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int main() {
+    cin >> n;
+    for(int i=0;i<n;i++){
+        for(int j=n-i-1;j>0;j--){
+            cout<<" ";
+        }
+        for(int j=0;j<2*(i+1)-1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=1;i<n;i++){
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        for(int j=2*(n-i)-1;j>0;j--){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    // Please write your code here.
+
+    return 0;
+}
