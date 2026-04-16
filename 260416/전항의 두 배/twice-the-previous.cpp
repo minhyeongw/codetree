a0292cc698c2bbf867e2c91876ef616d97270f11
민형 <wemin05@gmail.com>
2026-04-16 18:56:42 +0900

    Title: 전항의 두 배, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260416/전항의 두 배/twice-the-previous.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10];
+    cin>>arr[0]>>arr[1];
+    for(int i=2;i<10;i++){
+        arr[i]=arr[i-1]+2*arr[i-2];
+    }
+
+    for(int i=0;i<10;i++){
+        cout<<arr[i]<<" ";
+        
+    }
+    // Please write your code here.
+    return 0;
+}
